52b75d140ac06dc13bfe54a8e23beb48bafe257c
Hayato Sakai <hayato999sp@gmail.com>
2025-08-17 13:48:00 +0900

    update logic.py

--- logic.py
+++ logic.py
@@ -2,14 +2,11 @@ import json
 import random
 import os
 
-# このスクリプト(logic.py)が存在するディレクトリの絶対パスを取得
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# data.jsonへの絶対パスを構築
-DATA_FILE_PATH = os.path.join(BASE_DIR, 'data.json')
+# 変更点：単純に、プロジェクトのルートからのパスを指定する
+DATA_FILE_PATH = 'data.json'
 
 def load_data():
-    """ 同梱された初期データを、絶対パスで読み込む """
-    # 変更点：絶対パスでファイルを開く
+    """ 同梱された初期データを読み込む """
     with open(DATA_FILE_PATH, 'r', encoding='utf-8') as f:
         return json.load(f)
 
